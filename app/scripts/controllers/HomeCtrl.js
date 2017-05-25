f1da49d4c80ecfddb461be8eb382dff1a9cc3186
Paul Sanford <sanfordpaul@gmail.com>
2017-05-24 20:38:33 -0500

    checkpoint 6 completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,33 +1,47 @@
 (function() {
-    function HomeCtrl(Room, $uibModal, Message, $cookies, $scope) {
+    function HomeCtrl(Room, $uibModal, Message, $cookies, $scope, $filter, $interval) {
         var home = this;
         home.rooms = Room.all;
         home.currentRoom = {};
         home.currentRoomMessages = {};
         home.currentUser = {};
         home.currentUser.name = $cookies.get('blocChatCurrentUser');
+        home.currentDateTime = {};
 
         $scope.$on('newUserCookie', function(eventObject, newUserName){
             console.log("in $on function, eventObject, newUsername =", eventObject, newUserName)
             home.currentUser.name = newUserName;
-        })
+        });
+
+        home.setDateTime = function() {
+            home.currentDateTime.value = Date.now();
+            home.currentDateTime.filtered = $filter('date')(home.currentDateTime.value, "medium");
+        };
+
+        $interval(home.setDateTime, 1000);
+
+
 
         home.addRoom = function() {
             $uibModal.open({
                     templateUrl: '/templates/modal.html',
                     controller: 'ModalCtrl as modal',
-                })
-        }
+                });
+        };
 
         home.roomClicked = function(roomObject) {
             home.currentRoom = roomObject;
             Message.getMessagesByRoomId(home.currentRoom.roomId);
             home.currentRoomMessages = Message.messages;
 
-        }
+        };
+        home.newMessage = function(newMessageContent) {
+            Message.newMessage(newMessageContent, home.currentRoom, home.currentDateTime, home.currentUser.name);
+
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl',  ['Room', '$uibModal', 'Message', '$cookies', '$scope', HomeCtrl]);
+        .controller('HomeCtrl',  ['Room', '$uibModal', 'Message', '$cookies', '$scope', '$filter','$interval', HomeCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,12 +1,24 @@
 (function() {
   function Message($firebaseArray) {
     var Message = {};
-    var ref = firebase.database().ref().child("messages");
+
 
     Message.getMessagesByRoomId = function(roomId) {
-        var query = ref.orderByChild("roomId").equalTo(roomId);
+        var ref = firebase.database().ref().child("messages");
+        var query = ref.orderByChild("roomObject/roomId").equalTo(roomId);
         Message.messages = $firebaseArray(query);
     };
+
+    Message.newMessage = function(newMessageContent, newMessageRoomId, newMessageTimeSent, newMessageUserName,) {
+        var newMessageObject = {
+            content: newMessageContent,
+            roomObject: newMessageRoomId,
+            timeSentObject: newMessageTimeSent,
+            userName: newMessageUserName
+        };
+        var ref = firebase.database().ref().child("messages");
+        ref.push(newMessageObject)
+    }
 
     return Message;
   }
